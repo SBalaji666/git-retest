cf23a82c341d2b1e6ef22169af5b092a422fb904
SBalaji666 <soomaja10@gmail.com>
2023-06-06 16:58:16 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,114 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const fs = require("fs");
+
+app.use(express.json());
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get("/", (req, res) => {
+  res.status(200).send("Hello from the server side");
+});
+
+app.post("/", (req, res) => {
+  res.status(200).send("You can post to this endpoint");
+});
+
+app.get("/api/v1/tours", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+app.post("/api/v1/tours", (req, res) => {
+  console.log(req.body);
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: "success",
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+});
+
+app.get("/api/v1/tours/:id", (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: "failed",
+      message: "Invalid ID",
+    });
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      tour,
+    },
+  });
+});
+
+app.patch("/api/v1/tours/:id", (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: "failed",
+      message: "Invalid ID",
+    });
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      tour: "Updated tour here...",
+    },
+  });
+});
+
+app.delete("/api/v1/tours/:id", (req, res) => {
+  const newTour = tours.filter((el) => el.id !== req.params.id * 1);
+
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: "failed",
+      message: "Invalid ID",
+    });
+  }
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(newTour),
+    (err) => {
+      res.status(200).json({
+        status: "success",
+        data: {
+          tour: "Deleted tour successfully...",
+        },
+      });
+    }
+  );
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log("Listening to port 3000");
+});
